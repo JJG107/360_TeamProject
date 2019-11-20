a78fb4ff8ece1d69e8cede3b3a9a2f33d8fd22d5
Michael <cai.michael.r@gmail.com>
2019-11-20 16:49:48 -0700

    Initial File

--- GradeAnalytics/src/Main.java
+++ GradeAnalytics/src/Main.java
@@ -5,31 +5,36 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
-public class Main extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
 
- Scene scene = new Scene(root, 300, 250);
+public class Main extends Application 
+{
 
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
- public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		Button btn = new Button();
+		btn.setText("Sample Button");
+		btn.setOnAction(new EventHandler<ActionEvent>() 
+		{
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Some Calculation");
+			}
+		});
+
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+
+		// Dimensions Width by Height
+		Scene scene = new Scene(root, 1200, 600);
+
+		primaryStage.setTitle("Grade Analytics");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
 }
