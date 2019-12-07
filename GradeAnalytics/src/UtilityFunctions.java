817c5f48e43d7b0be637bc6c74d96fb3aa3b18e3
pr2a <31780665+pr2a@users.noreply.github.com>
2019-12-06 20:29:43 -0700

    wip2

--- GradeAnalytics/src/UtilityFunctions.java
+++ GradeAnalytics/src/UtilityFunctions.java
@@ -53,103 +53,96 @@ public class UtilityFunctions {
 	String title;
 	FontMetrics fm;
 	int dist1, dist2, dist3, dist4, dist5, dist6, dist7, dist8, dist9, dist10;
+	int highestScore;
 
-	   
-	   //distribution1
-for (int i = 1; i < dist1; i++) {
-	if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	   //distribution2
-	   for (int i = 1; i < dist2; i++)
-	   {
-	      if (i % 10 == 0)
-	      {
-	         System.out.print("X");
-	      }
-	      System.out.print("*");
-	   }
-	   
-	   //distribution 3
-	   for (int i = 1; i < dist3; i++) {
-		      if (i % 10 == 0){
-		         System.out.print("X");
-		      }
-		      System.out.print("*");                
-		   }
-	   
-	 //distribution4
-	   for (int i = 1; i < dist4; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	 //distribution5
-	   for (int i = 1; i < dist5; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	 //distribution6
-	   for (int i = 1; i < dist6; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	 //distribution7
-	   for (int i = 1; i < dist7; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	 //distribution8
-	   for (int i = 1; i < dist8; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	 //distribution9
-	   for (int i = 1; i < dist9; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-	 //distribution10
-	   for (int i = 1; i < dist10; i++) {
-	      if (i % 10 == 0){
-	         System.out.print("X");
-	      }
-	      System.out.print("*");                
-	   }
-	   
-
-	   System.out.println("\n\nBar Chart");
-	   System.out.println("Chart 1: " + dist1);
-	System.out.println("Chart 2: " + dist2);
-	System.out.println("Chart 3: " + dist3);
-	System.out.println("Chart 4: " + dist4);
-	System.out.println("Chart 5: " + dist5);
-	System.out.println("Chart 6: " + dist6);
-	System.out.println("Chart 7: " + dist7);
-	System.out.println("Chart 8: " + dist8);
-	System.out.println("Chart 9: " + dist9);
-	System.out.println("Chart 10: " + dist10);
-	    
+	public void makeGraph(int highestScore, int count, int dist1, int dist2, int dist3, 
+		int dist4, int dist5, int dist6, int dist7, int dist8, int dist9, int dist10) {
+		System.out.println("Each X represents 10 students.");
+		System.out.println("----------------------------------------------------");
+		
+		int maxX = 30;
+		
+		//distribution1
+		for (int i = 1; i <= dist1; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist1");
+		}
+		
+		//distribution2
+		for (int i = 1; i <= dist2; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist2");
+		}
+		
+		//distribution3
+		for (int i = 1; i <= dist3; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist3");
+		}
+		
+		//distribution4
+		for (int i = 1; i <= dist4; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist4");
+		}
+		
+		//distribution5
+		for (int i = 1; i <= dist5; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist5");
+		}
+		
+		//distribution6
+		for (int i = 1; i <= dist6; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist6");
+		}
+		
+		//distribution7
+		for (int i = 1; i <= dist7; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist7");
+		}
+		
+		//distribution8
+		for (int i = 1; i <= dist8; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist8");
+		}
+		
+		//distribution9
+		for (int i = 1; i <= dist9; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist9");
+		}
+		
+		//distribution10
+		for (int i = 1; i <= dist10; i++) {
+			int numX = count/highestScore * maxX;
+			for(int k = 1; k <= numX; k++)
+				System.out.print("X");
+			System.out.println("dist10");
+		}
+		
+		System.out.println("\n\nGrade Analytics Bar Chart");
 	 }
 	   
 
@@ -228,4 +221,71 @@ for (int i = 1; i < dist1; i++) {
 		}
 		return toReturn;
 	}
+	
+	
+	
+	public void JXCanvas(int[] x, String[] y, String a) {
+		
+		this.title = title;
+		
+	    for(int i = 0; i < x.length; i++){
+	        if(x[i] > max)
+	            max = x[i];
+	    }
+	    
+	    nums = x;
+	    cat = y;
+	    t = a;
+	    width = nums.length*100 + nums.length*10;
+	    height = max*10 + 500;
+	    setSize(width,height);
+	}
+	
+	private void setSize(int width2, int height2) {
+		// TODO Auto-generated method stub
+	}
+
+	//paints the bar graph
+	public void paint(Graphics g, int width) {
+		fm = getFontMetrics(getFont());
+		titleHeight = fm.getHeight();
+		g.drawString(title, (width - fm.stringWidth(title))/2, top);
+		
+		//draws the min and max values
+		g.drawString(new Integer(min).toString(), padding, bottom);
+	    g.drawString(new Integer(max).toString(), padding, top + titleHeight);
+	}
+	
+	private Object size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private FontMetrics getFontMetrics(Object font) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Object getFont() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void clearGraph() {
+	}
+	
+	void bars(Graphics g, double x1, double y1, double x2, double y2, int width, int height) {
+        // Draw a bar from the point (x1,y1) to (x2,y2).
+        
+     int a1, b1;   // pixels
+     int a2, b2;   // pixels
+     
+     a1 = (int)( (x1 + 5) / 10 * width );
+     b1 = (int)( (5 - y1) / 10 * height );
+     a2 = (int)( (x2 + 5) / 10 * width );
+     b2 = (int)( (5 - y2) / 10 * height );
+     
+     g.drawLine(a1,b1,a2,b2);
+     
+  }
 }
